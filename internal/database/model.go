1e96465da8509f38f919398e8aaed5f467ffda6e
augustine <augustino0890@gmail.com>
2023-01-19 18:58:51 +0900

    stats-bot: sentiment analysis with huggingface models

--- internal/database/model.go
+++ internal/database/model.go
@@ -5,11 +5,13 @@ import (
 )
 
 type Message struct {
-	ID        primitive.ObjectID `bson:"_id"`
-	Username  string             `bson:"username"`
-	Channel   string             `bson:"channel"`
-	Text      string             `bson:"text"`
-	Sentiment string             `bson:"sentiment"`
+	ID                   primitive.ObjectID `bson:"_id"`
+	Username             string             `bson:"username"`
+	Channel              string             `bson:"channel"`
+	Text                 string             `bson:"text"`
+	Sentiment            string             `bson:"sentiment"`
+	SentimentHuggingFace string             `bson:"huggingface,omitempty"`
+	Emotion              string             `bson:"emotion,omitempty"`
 	// SentimentScore map[string]float64 `bson:"sentiment_score,omitempty"`
 	// Topic          string             `bson:"topic,omitempty"`
 	CreatedAt primitive.DateTime `bson:"createdAt"`
